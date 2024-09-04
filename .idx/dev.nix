e59262b50b9119dc3e6551cde4837b9f4932f1cc
lightningboy78 <lightningboy78@gmail.com>
2024-09-04 00:16:09 +0000

    Nothing To Update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -19,6 +19,9 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "ms-python.python"
+      "ms-python/debugpy"
+
     ];
 
     # Enable previews
